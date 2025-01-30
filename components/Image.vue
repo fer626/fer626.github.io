a69afd8f7b204f8f2a8acb24ea3bcacd9e7bdde9
Neko Quincy <fernando.leon92@gmail.com>
2025-01-29 18:49:54 -0600

    feat: new components added

---
+++ components/Image.vue
@@ -0,0 +1,13 @@
+<template>
+	<img :src="url" :alt="alt" class="w-full h-full object-fill" />
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const alt = ref('una imagen random')
+const url = computed(() => {
+	return '' || '/assets/img/default.png'
+})
+
+</script>
