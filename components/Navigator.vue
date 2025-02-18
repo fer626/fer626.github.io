fde4f7bc6c908fb0e7802f389bbf474e82df8c9b
Neko Quincy <fernando.leon92@gmail.com>
2025-02-17 18:51:00 -0600

    feat: created Navigator

---
+++ components/Navigator.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { reactive } from 'vue'
+const props = defineProps(() => ({
+	links: {
+		type: Array,
+		default: () => []
+	}
+}))
+
+const _links = [
+	{ title: 'Home', url: '/' },
+	{ title: 'About us', url: '/about-us' },
+	{ title: 'Contact me', url: '/contact-me' },
+	{ title: 'Guarantee', url: '/guarantee' },
+	{ title: 'Policy', url: '/policy' },
+] 
+</script>
+
+<template>
+	<div class="bg-red-300">
+		<ul class="flex gap-[10px]">
+			<li v-for="link in _links">
+				<NuxtLink :to="link.url">{{ link.title }}</NuxtLink>
+			</li>
+		</ul>
+	</div>
+</template>
